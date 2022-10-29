817cc49ecb421619860a380220b8ea9c8768b778
stdio <SGDiomov18@codingburgas.bg>
2022-10-29 12:43:11 +0300

    SVG implementation

--- src/main/java/photozaruvaj/photozaruvaj/PhotozaruvajApplication.java
+++ src/main/java/photozaruvaj/photozaruvaj/PhotozaruvajApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PhotozaruvajApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(PhotozaruvajApplication.class, args);
     }
 
